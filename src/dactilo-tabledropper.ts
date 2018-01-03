f8a1887f763ea418af2d7641e96598839eecc22e
Quentin PÂRIS <git@quentin.paris>
2018-01-03 23:22:04 +0100

    Support of scope

--- src/dactilo-tabledropper.ts
+++ src/dactilo-tabledropper.ts
@@ -1,26 +1,53 @@
 import angular = require("angular");
 import {TableDraggerModule} from "./table-dragger/index";
+import {IScope, ITimeoutService} from "angular";
 
 export module DactiloTableDropperModule {
-    let TableDirective = function (): angular.IDirective {
+    interface TableDirectiveScope extends IScope {
+        dtdModel: any;
+    }
+
+    let TableDirective = function($timeout: ITimeoutService): angular.IDirective {
         return {
             transclude: false,
-            scope: {},
+            scope: {
+                dtdModel: '='
+            },
             restrict: 'A',
-            link: function(scope: angular.IScope,
+            link: function(scope: TableDirectiveScope,
                            element: any) {
-                TableDraggerModule.tableDragger(element[0], {
-                    mode: "free",
-                    onlyBody: true,
-                    dragHandler: '.handle'
-                }).on('shadowMove', (oldIndex, newIndex, el, mode) => {
-                    console.log('shadowMove');
-                    console.log(newIndex);
+                let arrayMove = function(arr, fromIndex, toIndex) {
+                    let element = arr[fromIndex];
+                    arr.splice(fromIndex, 1);
+                    arr.splice(toIndex, 0, element);
+                };
+
+                $timeout(function () {
+                    TableDraggerModule.tableDragger(element[0], {
+                        mode: "free",
+                        onlyBody: true,
+                        dragHandler: '.handle'
+                    }).on('drop', (oldIndex, newIndex, el, mode) => {
+                        if(mode == "row") {
+                            arrayMove(scope.dtdModel.lines, oldIndex - 1, newIndex - 1);
+                            scope.$apply();
+                        }
+
+                        if(mode == "column") {
+                            arrayMove(scope.dtdModel.header, oldIndex, newIndex );
+
+                            scope.dtdModel.lines.forEach(line => {
+                                arrayMove(line, oldIndex, newIndex);
+                            });
+                            scope.$apply();
+                        }
+                    });
                 });
+
             }
         }
     };
 
     export let angularModule = angular.module('dactilo-tabledropper', [])
-        .directive('dtdTable', [TableDirective]);
+        .directive('dtdTable', ["$timeout", TableDirective]);
 }
